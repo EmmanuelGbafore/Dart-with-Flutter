d7c5eb6dcd2172b58bb565a738c39d987584d0b6
Emmanuel Gbafore <egbafore@gmail.com>
2024-03-18 15:45:07 +0300

    Add Data_types.dart

---
+++ Data_types.dart
@@ -0,0 +1,21 @@
+// Example of Integer
+void main(){
+int age = 31;
+
+// Example of Double
+double height = 6.1;
+
+// Example of String
+String name = "Emmanuel Gbafore";
+
+// Example of List
+List <String> counties = ["Kiambu", "Mombassa", "Nakuru", "Uasin Gishu"];
+
+// Example of Map
+Map<String, String> capitals = {'Tana River': 'Hola', 'Uasin Gishu': 'Eldoret', 'West Pokot': 'Kapengura', 'Kiambu':'Kiambu Town'};
+
+// Code to print out the various outputs.
+
+print("Hi, I would like to introduce myself.");
+print("My name is $name. I am $age years old. And my height is $height feet. I am from ${counties[0]}. The capital of my county is ${capitals['Kiambu']}" );
+}
