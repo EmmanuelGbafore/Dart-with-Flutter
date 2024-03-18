c225674076fd058f098a51fc83675d488c531ffe
Emmanuel Gbafore <egbafore@gmail.com>
2024-03-18 22:05:49 +0300

    Add Control_flow.dart file

---
+++ Control_flow.dart
@@ -0,0 +1,14 @@
+void main() {
+  print("Enter number: ");
+ // var number = 10;         // To print out Your number is equal to 10;
+  var number = 15;          // To print out Your number is greater than 10;
+ // var number = 5;            // To print out Your number is less than 10
+  print(number);
+      if (number > 10) {
+        print('Your number is greater than 10');
+      } else if (number == 10) {
+        print('Your number is equal to 10');
+      } else {
+        print('Your number is less than 10');
+      }
+  }
